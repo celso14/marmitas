b2849e868e2ef72ac91035f87ab321b4cc422b25
Bcelso14 <botelhocelso99@gmail.com>
2023-09-11 15:39:53 -0300

    feat: patch mapping

--- src/main/java/com/api/marmitas/controllers/CostumerController.java
+++ src/main/java/com/api/marmitas/controllers/CostumerController.java
@@ -6,9 +6,9 @@ import org.springframework.http.HttpStatus;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -48,7 +48,7 @@ public class CostumerController {
         return this.costumerService.create(costumerCreateDTO);
     }
 
-    @PutMapping("/{id}")
+    @PatchMapping("/{id}")
     @ResponseStatus(code = HttpStatus.ACCEPTED)
     public CostumerOutputDTO update(@PathVariable @NotNull @Positive Long id, @RequestBody @Validated CostumerUpdateDTO costumerUpdateDTO) {
         return this.costumerService.update(id, costumerUpdateDTO);
